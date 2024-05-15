f625aea8070d74ded93968361544603db5844f23
datnq2001 <datnq2001@gmail.com>
2024-05-15 23:26:42 +1000

    1 commit

---
+++ src/Components/HomeScreen/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="grid-footer-Author">&copy;2024 Trendall Research Centre</div>
+    </footer>
+  );
+}
+
+export default Footer;
